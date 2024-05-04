4f3d70e3ae1c5dc01ce641df65c3c8a45b041dc1
hazem_m.o <hm3185@fayoum.edu.eg>
2024-05-04 18:35:52 +0300

    add new Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Task from './Task.jsx'
+
+// Application Component
+export function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/App" element={<App />}></Route>
+        <Route path="/Task" element={<Task />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Application />
+  </React.StrictMode>
+);
